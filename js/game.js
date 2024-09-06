f3b97213365b196e7b6acbdd702b17c04bd1c2f4
Patrick Schlieker <patrick.schlieker@gmail.com>
2024-09-06 17:30:43 +0200

    splashscreens done

--- js/game.js
+++ js/game.js
@@ -6,12 +6,25 @@ let music = new Music(true);
 let allIntervals = [];
 let animationFrame;
 let overlay;
+let startImg = new Image();
+startImg.src = "../img/6_Botones/startsplash.png";
+let winImg = new Image();
+winImg.src = "../img/6_Botones/Titles/You win/Mesa de trabajo 1.png";
+let looseImg = new Image();
+looseImg.src = "../img/6_Botones/Titles/Game Over/Recurso 9.png";
 
 function init() {
-  toggleGameKeyListeners("on");
   canvas = document.getElementById("canvas");
   ctx = canvas.getContext("2d");
   overlay = document.getElementById("overlay");
+  showSplashStartHelper();
+}
+
+function startGame() {
+  window.removeEventListener("keydown", startListener);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  overlay.innerHTML = "";
+  toggleGameKeyListeners("on");
   initLevels(canvas);
   world = new World(canvas, keyboard);
 }
@@ -38,37 +51,55 @@ function gameOver(win) {
   }, 200);
 }
 
+function showSplashStartHelper() {
+  showSplashStart();
+  setTimeout(() => {
+    showSplashStart();
+  }, 500);
+}
+
 function showSplashStart() {
-  ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+  ctx.drawImage(startImg, 0, 0, canvas.width, canvas.height);
+  displayStartButton();
 }
 
 function showSplashWin() {
-  let img = new Image();
-  img.src = "../img/6_Botones/Titles/You win/Mesa de trabajo 1.png";
-  ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+  ctx.drawImage(winImg, 0, 0, canvas.width, canvas.height);
 }
 
-function showSplashLoose() {}
+function showSplashLoose() {
+  ctx.drawImage(looseImg, 100, 200, canvas.width - 200, canvas.height - 300);
+}
 
 function displayRestartButton() {
-  window.addEventListener("keydown", (e) => {
-    if (e.code === "Space") {
-      restartGame();
-    }
-  });
+  window.addEventListener("keydown", restartListener);
   overlay.innerHTML = `<button id="restart" onclick="restartGame()">Restart</button><br>
   <h3>or Press Space to restart</h3>`;
 }
 
+function displayStartButton() {
+  window.addEventListener("keydown", startListener);
+  overlay.innerHTML = `<button id="restart" onclick="startGame()">Start</button><br>
+  <h3>or Press Space to start</h3>`;
+}
+
 function restartGame() {
-  window.removeEventListener("keydown", (e) => {
-    if (e.code === "Space") {
-      restartGame();
-    }
-  });
+  window.removeEventListener("keydown", restartListener);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   overlay.innerHTML = "";
-  console.log("Restart");
+  showSplashStart();
+}
+
+function startListener(e) {
+  if (e.code === "Space") startGame();
+  if (e.code === "KeyM") music.toggleMusic();
+  if (e.code === "KeyN") music.toggleSound();
+}
+
+function restartListener(e) {
+  if (e.code === "Space") {
+    restartGame();
+  }
 }
 
 function stopIntervals() {
@@ -88,11 +119,11 @@ function handleKeyUpWrapper(e) {
 
 function toggleGameKeyListeners(state) {
   if (state === "on") {
-    window.addEventListener("keydown", handleKeyDownWrapper);
-    window.addEventListener("keyup", handleKeyUpWrapper);
+    window.addEventListener("keydown", handleKeyDown);
+    window.addEventListener("keyup", handleKeyUp);
   } else {
-    window.removeEventListener("keydown", handleKeyDownWrapper);
-    window.removeEventListener("keyup", handleKeyUpWrapper);
+    window.removeEventListener("keydown", handleKeyDown);
+    window.removeEventListener("keyup", handleKeyUp);
   }
 }
 
